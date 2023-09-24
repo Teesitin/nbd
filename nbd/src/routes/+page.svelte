1e1189a17b8891769cea9b2f0ccb1cceda83075a
Teesitin <teesitin.russell@gmail.com>
2023-09-24 11:54:36 -0400

    Looking for a quick theme

--- nbd/src/routes/+page.svelte
+++ nbd/src/routes/+page.svelte
@@ -1,9 +1,25 @@
 <h1 class="text-3xl font-bold">
     Hello world!
-  </h1>
+</h1>
   
 <style lang="postcss">
-    :global(html) {
-      background-color: #ffffff;
-    }
+
+:root {
+    --cloud-burst-50: #f1f5fd;
+    --cloud-burst-100: #dfe8fa;
+    --cloud-burst-200: #c6d6f7;
+    --cloud-burst-300: #9ebcf2;
+    --cloud-burst-400: #7099ea;
+    --cloud-burst-500: #4f76e2;
+    --cloud-burst-600: #3a59d6;
+    --cloud-burst-700: #3147c4;
+    --cloud-burst-800: #2d3ba0;
+    --cloud-burst-900: #2a367e;
+    --cloud-burst-950: #202654;
+}
+
+:global(html) {
+    background-color: var(--cloud-burst-950);
+    color: white;
+}
 </style>
