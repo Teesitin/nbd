5f422535155d242c25d21f5e1e41a308507c9651
Teesitin <teesitin.russell@gmail.com>
2023-09-24 20:07:29 -0400

    seachbar

--- nbd/src/routes/+page.svelte
+++ nbd/src/routes/+page.svelte
@@ -1,25 +1,91 @@
-<h1 class="text-3xl font-bold">
-    Hello world!
-</h1>
-  
-<style lang="postcss">
+<script>
+    import { CalendarWeekSolid } from 'flowbite-svelte-icons';
 
-:root {
-    --cloud-burst-50: #f1f5fd;
-    --cloud-burst-100: #dfe8fa;
-    --cloud-burst-200: #c6d6f7;
-    --cloud-burst-300: #9ebcf2;
-    --cloud-burst-400: #7099ea;
-    --cloud-burst-500: #4f76e2;
-    --cloud-burst-600: #3a59d6;
-    --cloud-burst-700: #3147c4;
-    --cloud-burst-800: #2d3ba0;
-    --cloud-burst-900: #2a367e;
-    --cloud-burst-950: #202654;
-}
 
-:global(html) {
-    background-color: var(--cloud-burst-950);
+</script>
+
+<style lang="scss">
+      @import url('https://fonts.googleapis.com/css2?family=Telex&display=swap');
+
+    :root {
+        --background: #2c2627;
+        --action: #dc142f;
+        --text: #ededed;
+
+    }
+
+    :global(html) {
+        background-color: var(--background);
+        color: var(--text);
+        font-family: 'Telex', Arial, Helvetica, sans-serif;
+    }
+
+    .search-bar {
+        width: 300px;
+        margin: 20px;
+        padding: 10px;
+        font-size: 16px;
+        color: black;
+        background-color: rgba(0, 0, 0, 0.1);
+
+        border: none;
+        outline: none;
+        border-radius: 9999px;
+
+        transition: background-color 0.3s ease;
+    }
+
+    .search-bar:focus {
+        background-color: rgba(0, 0, 0, 0.1);
+    }
+
+
+
+/* Seach Bar */
+@import url('https://fonts.googleapis.com/css?family=Inconsolata:700');
+
+
+.search-bar {
+
+    padding: 10px;
+    font-size: 16px;
+    font-family: 'Inconsolata';
     color: white;
-}
+    background-color: rgba(255, 255, 255, 0.1);
+    border: none;
+    outline: none;
+    border-radius: 30px;
+    transition: background-color 0.3s ease;
+
+    &:focus {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    &::placeholder {
+      color: white;
+      opacity: 0.5;
+    }
+  }
+
+
 </style>
+
+<div class="w-screen h-screen flex justify-center flex-col items-center	border">
+
+    <!-- Hero -->
+    <h1 class="text-5xl font-bold m-2">
+        Nothing But Docs
+    </h1>
+    <h2 class="m-2">
+        Celebrating good documentation
+    </h2>
+    
+    <!-- Doc Search -->
+    <div class="search-container">
+        <input type="text" class="search-bar" placeholder="Search...">
+
+        <iconify-icon icon="mdi:home"></iconify-icon>
+
+    </div>
+
+</div>
