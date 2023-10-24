e23ba386577226149fb66f06f7cbb19f4776f7b9
Teesitin <teesitin.russell@gmail.com>
2023-10-23 21:42:22 -0400

    starting to make a supabase login

--- nbd/src/lib/components/NavBar.svelte
+++ nbd/src/lib/components/NavBar.svelte
@@ -7,14 +7,16 @@
 <Navbar let:hidden let:toggle rounded color="form" class="bg-transparent text-scorpion-950  dark:bg-scorpion-950">
     <NavBrand href="/">
 
-      <span class="self-center whitespace-nowrap text-xl font-semibold ">NBD</span>
+      <span class="whitespace-nowrap text-xl font-semibold ">NBD</span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
-    <NavUl {hidden} class="items-center">
+    <NavUl {hidden} class="flex items-center">
       <NavLi href="/" active={true} class="text-scorpion-950">Home</NavLi>
       <NavLi href="/about" class="text-scorpion-950">About</NavLi>
       <NavLi href="/donate" class="text-scorpion-950">Donate</NavLi>
       <NavLi href="/faq" class="text-scorpion-950">FAQ</NavLi>
+      <NavLi href="/login" class="text-scorpion-950">Login</NavLi>
+
       <DarkMode />
 
     </NavUl>
