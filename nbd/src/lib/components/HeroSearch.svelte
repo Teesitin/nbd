19ec914d661baf10b80fa21db5c9bca56c79d351
Teesitin <teesitin.russell@gmail.com>
2023-10-23 21:05:23 -0400

    added darkmode and removed methodlology

--- nbd/src/lib/components/HeroSearch.svelte
+++ nbd/src/lib/components/HeroSearch.svelte
@@ -7,11 +7,11 @@
 <div class="w-full h-96 flex justify-center flex-col items-center">
 
     <!-- Hero -->
-    <h1 class="text-5xl font-bold m-2 text-scorpion-950 text-center">
+    <h1 class="text-5xl font-bold m-2 text-scorpion-950 text-center dark:text-scorpion-50">
         Nothing But Docs
     </h1>
 
-    <h2 class="m-2 text-scorpion-950">
+    <h2 class="m-2 text-scorpion-950 dark:text-scorpion-50">
         Celebrating good documentation
     </h2>
     
@@ -19,8 +19,8 @@
     <div class="search-container m-4 flex justify-center items-center border-violet-950 border-solid gap-4 flex-col  w-72 sm:flex-row sm:w-96">
         <input type="text" class="search-bar w-full sm:w-5/6 font-roboto boder-scorpion-950 border-2 rounded-full" placeholder="Search for Software...">
 
-        <div class="w-10 h-10 bg-scorpion-950 rounded-full flex justify-center items-center transform transition-transform duration-300 hover:scale-110 cursor-pointer">
-            <svg class="w-6 h-6 p-1 text-scorpion-50  dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <div class="w-10 h-10 bg-scorpion-950 rounded-full flex justify-center items-center transform transition-transform duration-300 hover:scale-110 cursor-pointer dark:bg-scorpion-50">
+            <svg class="w-6 h-6 p-1 text-scorpion-50  dark:text-scorpion-950" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
             </svg>
         </div>
--- nbd/src/lib/components/NavBar.svelte
+++ nbd/src/lib/components/NavBar.svelte
@@ -1,19 +1,21 @@
 <script>
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+    import { DarkMode } from 'flowbite-svelte';
 </script>
 
 
-<Navbar let:hidden let:toggle rounded color="form" class="bg-transparent text-scorpion-950">
+<Navbar let:hidden let:toggle rounded color="form" class="bg-transparent text-scorpion-950  dark:bg-scorpion-950">
     <NavBrand href="/">
 
       <span class="self-center whitespace-nowrap text-xl font-semibold ">NBD</span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
-    <NavUl {hidden}>
+    <NavUl {hidden} class="items-center">
       <NavLi href="/" active={true} class="text-scorpion-950">Home</NavLi>
-      <NavLi href="/methodology" class="text-scorpion-950">Methodology</NavLi>
       <NavLi href="/about" class="text-scorpion-950">About</NavLi>
       <NavLi href="/donate" class="text-scorpion-950">Donate</NavLi>
       <NavLi href="/faq" class="text-scorpion-950">FAQ</NavLi>
+      <DarkMode />
+
     </NavUl>
 </Navbar>
