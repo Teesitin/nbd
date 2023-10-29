0dc6836efd8db2490673ef40f461917c98c5d1a2
Teesitin <teesitin.russell@gmail.com>
2023-10-29 19:36:29 -0400

    Fixed Page Base Error

--- nbd/src/routes/software/unreal/+page.svelte
+++ nbd/src/routes/software/unreal/+page.svelte
@@ -1,11 +1,9 @@
 <script>
-    import PageBase from "$lib/PageBase.svelte";
     import SoftwareBase from "$lib/components/SoftwareBase.svelte";
 </script>
 
 
-<PageBase>
+
     <SoftwareBase>
 
     </SoftwareBase>
-</PageBase>
