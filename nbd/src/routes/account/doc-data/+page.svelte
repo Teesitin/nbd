18d39f4a10ac5534a3f8e5a97ab34adf2db75ad2
Teesitin <teesitin.russell@gmail.com>
2023-10-29 21:08:06 -0400

    Dynamic Data adding through a modal

--- nbd/src/routes/account/doc-data/+page.svelte
+++ nbd/src/routes/account/doc-data/+page.svelte
@@ -2,6 +2,18 @@
     import { createClient } from '@supabase/supabase-js';
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
+    import { Modal, Label, Input, Checkbox } from 'flowbite-svelte';
+    let formModal = false;
+    import { Textarea } from 'flowbite-svelte';
+
+    let title = '';
+    let description = '';
+    let url_link = '';
+    let rating: number | null = null;
+    let rating_comment = '';
+    let tags = '';
+    let category = '';
+
     
     import { Button, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
 
@@ -9,20 +21,24 @@
 
     let data: any[] = [];
 
-    const newDoc = {
-        title: "Sample Country",
-        description: "A sample description",
-        url_link: "https://sample-country-url.com",
-        rating: 5,
-        rating_comment: "Positive feedback",
-        tags: "tag1,tag2,tag3",
-        category: "Geography"
-    };
+   
+    async function addDoc(event: Event) {
 
-    
-    
-    async function addDoc() {
-        const { data, error } = await supabase.from('doc_data').insert([newDoc]);
+        event.preventDefault(); // Prevent form from refreshing the page
+
+        const doc = {
+            title,
+            description,
+            url_link,
+            rating,
+            rating_comment,
+            tags,
+            category
+        };
+
+
+
+        const { data, error } = await supabase.from('doc_data').insert([doc]);
         
         if (error) {
             console.error("Error inserting data:", error);
@@ -43,33 +59,83 @@
     });
 </script>
 
-<Button on:click={addDoc}>Add Doc</Button >
 
 <div class="max-w-7xl m-auto p-2">
-  <Table striped={true}>
-    <TableHead>
-      <TableHeadCell>Title</TableHeadCell>
-      <TableHeadCell>Description</TableHeadCell>
-      <TableHeadCell>URL Link</TableHeadCell>
-      <TableHeadCell>Rating</TableHeadCell>
-      <TableHeadCell>Tags</TableHeadCell>
-      <TableHeadCell>Category</TableHeadCell>
-      <TableHeadCell>Action</TableHeadCell>
-    </TableHead>
-  <TableBody class="divide-y">
-      {#each data as row}
-        <TableBodyRow>
-          <TableBodyCell>{row.title}</TableBodyCell>
-          <TableBodyCell>{row.description}</TableBodyCell>
-          <TableBodyCell>{row.url_link}</TableBodyCell>
-          <TableBodyCell>{row.rating}</TableBodyCell>
-          <TableBodyCell>{row.tags}</TableBodyCell>
-          <TableBodyCell>{row.category}</TableBodyCell>
-          <TableBodyCell>
-            <a href="/edit/{row.id}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
-          </TableBodyCell>
-        </TableBodyRow>
-      {/each}
-    </TableBody>
-  </Table>
+    <Button on:click={() => (formModal = true)}>Add Docs</Button>
+    <Table striped={true}>
+        <TableHead>
+            <TableHeadCell>Title</TableHeadCell>
+            <TableHeadCell>Description</TableHeadCell>
+            <TableHeadCell>URL Link</TableHeadCell>
+            <TableHeadCell>Rating</TableHeadCell>
+            <TableHeadCell>Tags</TableHeadCell>
+            <TableHeadCell>Category</TableHeadCell>
+            <TableHeadCell>Action</TableHeadCell>
+        </TableHead>
+        <TableBody>
+            {#each data as row}
+            <TableBodyRow>
+                <TableBodyCell>{row.title}</TableBodyCell>
+                <TableBodyCell>{row.description}</TableBodyCell>
+                <TableBodyCell>{row.url_link}</TableBodyCell>
+                <TableBodyCell>{row.rating}</TableBodyCell>
+                <TableBodyCell>{row.tags}</TableBodyCell>
+                <TableBodyCell>{row.category}</TableBodyCell>
+                <TableBodyCell>
+                <a href="/edit/{row.id}" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Edit</a>
+                </TableBodyCell>
+            </TableBodyRow>
+            {/each}
+        </TableBody>
+    </Table>
 </div>
+
+<Modal bind:open={formModal} size="md" autoclose={false} class="w-full">
+    <form class="flex flex-col space-y-6" on:submit={addDoc}>
+        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Add New Document Data</h3>
+
+        <!-- URL Link -->
+        <Label class="space-y-2">
+            <span>URL Link</span>
+            <Input bind:value={url_link} type="url" name="url_link" placeholder="Enter URL link" />
+        </Label>
+
+        <!-- Title -->
+        <Label class="space-y-2">
+            <span>Title</span>
+            <Input bind:value={title} type="text" name="title" placeholder="Enter title" required />
+        </Label>
+
+        <!-- Description -->
+        <Label class="space-y-2">
+            <span>Description</span>
+            <Textarea bind:value={description} name="description" placeholder="Enter description" required></Textarea>
+        </Label>
+
+        <!-- Rating -->
+        <Label class="space-y-2">
+            <span>Rating</span>
+            <Input bind:value={rating} type="number" name="rating" placeholder="Enter rating (1-10)" min="1" max="10" />
+        </Label>
+
+        <!-- Rating Comment -->
+        <Label class="space-y-2">
+            <span>Rating Comment</span>
+            <Textarea bind:value={rating_comment} name="rating_comm" placeholder="Enter rating comment"></Textarea>
+        </Label>
+
+        <!-- Tags -->
+        <Label class="space-y-2">
+            <span>Tags</span>
+            <Input bind:value={tags} type="text" name="tags" placeholder="Enter tags (comma separated)" />
+        </Label>
+
+        <!-- Category -->
+        <Label class="space-y-2">
+            <span>Category</span>
+            <Input bind:value={category} type="text" name="category" placeholder="Enter category" />
+        </Label>
+
+        <Button type="submit" class="w-full">Add to Database</Button>
+    </form>
+</Modal>
