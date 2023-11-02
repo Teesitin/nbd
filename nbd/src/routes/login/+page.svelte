a135125d876b87848e96b2cf199961c49292bef3
Teesitin <teesitin.russell@gmail.com>
2023-11-02 11:07:12 -0400

    Updating Login

--- nbd/src/routes/login/+page.svelte
+++ nbd/src/routes/login/+page.svelte
@@ -1,30 +1,19 @@
 <script lang="ts">
   import { Section, Register } from 'flowbite-svelte-blocks';
-  import { Button, Checkbox, Label, Input } from 'flowbite-svelte';
+  import { Button, Checkbox, Label, Input, Modal, Popover } from 'flowbite-svelte';
+  import { CheckOutline, CloseOutline } from 'flowbite-svelte-icons';
+  let loginState = true;
 
-  import { Auth } from '@supabase/auth-ui-svelte';
-  import { ThemeSupa } from '@supabase/auth-ui-shared';
 
-  export let data;
+
+
+
+
+  
 </script>
-    
 
-<Section name="login">
-  <div class="row flex-center flex">
-    <div class="col-6 form-widget">
-      <Auth
-        supabaseClient={data.supabase}
-        view="magic_link"
-        redirectTo={`${data.url}/auth/callback`}
-        showLinks={false}
-        appearance={{ theme: ThemeSupa, style: { input: 'color: #fff' } }}
-      />
-    </div>
-  </div>
-</Section>
 
-<div class="m-auto w-96 text-center">↓↓↓ Make login look like this later! ↓↓↓</div>
-
+{#if loginState}
 <Section name="login">
   <Register href="/">
     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -38,12 +27,58 @@
           <span>Password</span>
           <Input type="password" name="password" placeholder="" required />
         </Label>
+        <div class="flex items-start">
+          <Checkbox>Remember me</Checkbox>
+          <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot password?</a>
+        </div>
 
-        <Button type="submit" class="w-full1">Send Confirmation Email</Button>
-
+        <div class="flex gap-6">
+          <Button type="button" color="alternative" class="w-1/2"  on:click={() => loginState = false}>Sign Up</Button>
+          <Button type="submit" class="w-1/2">Sign In</Button>
+        </div>
       </form>
     </div>
   </Register>
 </Section>
 
+{:else}
+<Section name="login">
+  <Register href="/">
+    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+      <form class="flex flex-col space-y-6" action="/">
+        <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Sign Up</h3>
 
+        <Label class="space-y-2">
+          <span>Email</span>
+          <Input type="email" name="email" placeholder="" required />
+        </Label>
+
+        <Label class="space-y-2">
+          <span>Password</span>
+          <Input type="password" name="password" placeholder="" required />
+        </Label>
+
+        <Label class="space-y-2">
+          <span>Username</span>
+          <Input type="password" name="password" placeholder="" required />
+        </Label>
+
+        <Label class="space-y-2">
+          <span>Full Name</span>
+          <Input type="password" name="password" placeholder="" />
+        </Label>
+
+        <Label class="space-y-2">
+          <span>Avatar URL</span>
+          <Input type="password" name="password" id="password" placeholder="" />
+        </Label>
+
+        <div class="flex gap-6">
+          <Button type="button" color="alternative" class="w-1/2" on:click={() => loginState = true}>Go Back</Button>
+          <Button type="submit" class="w-1/2">Create Account</Button>
+        </div>
+      </form>
+    </div>
+  </Register>
+</Section>
+{/if}
