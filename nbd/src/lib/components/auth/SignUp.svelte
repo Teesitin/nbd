e76582682f211d64ce575e02f0c899212ca0594c
Teesitin <teesitin.russell@gmail.com>
2023-11-03 19:52:57 -0400

    I think I got a sign UP!!!!

--- nbd/src/lib/components/auth/SignUp.svelte
+++ nbd/src/lib/components/auth/SignUp.svelte
@@ -2,7 +2,8 @@
     import { Section, Register } from 'flowbite-svelte-blocks';
     import { Button, Checkbox, Label, Input, Modal, Popover } from 'flowbite-svelte';
     import { createEventDispatcher } from 'svelte';
-    import { supabase } from '@supabase/auth-ui-shared';
+    import { supabase } from '$lib/supabaseClient';
+
 
     const dispatch = createEventDispatcher();
 
@@ -18,9 +19,13 @@
     let password = '';
     let passwordStrength = '';
 
+    let errorMessage: string = '';
+
+
     function checkPasswordStrength(pass: string) {
         let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})');
         let mediumPassword = new RegExp('^(?=.*[!@#$%^&*]).{8,}$');
+        errorMessage = '';
 
         if(strongPassword.test(pass)) {
         return 'Strong';
@@ -31,26 +36,33 @@
         }
     }
 
-    $: passwordStrength = checkPasswordStrength(password), console.log(password);
+    $: passwordStrength = checkPasswordStrength(password);
 
 
 
 
     // Sign Up
     async function signUpNewUser() {
+        errorMessage = '';
+
+        if (password.length < 6) {
+            errorMessage = 'Password must be at least 6 characters long';
+            return;
+        }
+
         const { data, error } = await (supabase as any).auth.signUp({
             email,
             password,
             options: {
-            redirectTo: 'https://example.com/welcome'
-        }
-    });
+            redirectTo: '/account/main'
+            }
+        }); 
     
-    if (error) {
-        console.error('Error signing up:', error);
-    } else {
-        console.log('Success! Check your email for the confirmation link.', data);
-    }
+        if (error) {
+            console.error('Error signing up:', error);
+        } else {
+            console.log('Success! Check your email for the confirmation link.', data);
+        }
     }
 </script>
 
@@ -61,35 +73,39 @@
             <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Sign Up</h3>
 
             <Label class="space-y-2">
-            <span>Email</span>
+                <span>Email</span>
             <Input type="email" name="emailNew" required bind:value={email}/>
             </Label>
 
             <Label class="space-y-2">
-            <span>Password</span>
-            <Input type="password" name="passwordNew" required bind:value={password} />
+                <span>Password</span>
+                <Input type="password" name="passwordNew" required bind:value={password} />
 
-            <p>Password strength: {passwordStrength}</p>
+                {#if errorMessage}
+                    <p class="text-red-500">{errorMessage}</p>
+                {:else}
+                    <p>Password strength: {passwordStrength}</p>
+                {/if}
             </Label>
 
             <Label class="space-y-2">
-            <span>Username</span>
-            <Input type="text" name="usernameNew" required bind:value={username}/>
+                <span>Username</span>
+                <Input type="text" name="usernameNew" required bind:value={username}/>
             </Label>
 
             <Label class="space-y-2">
-            <span>Full Name</span>
-            <Input type="text" name="fullName" placeholder="" bind:value={fullName}/>
+                <span>Full Name</span>
+                <Input type="text" name="fullName" placeholder="" bind:value={fullName}/>
             </Label>
 
             <Label class="space-y-2">
-            <span>Avatar URL</span>
-            <Input type="url" name="avatarUrl" id="password" placeholder="" bind:value={avatarUrl}/>
+                <span>Avatar URL</span>
+                <Input type="url" name="avatarUrl" id="password" placeholder="" bind:value={avatarUrl}/>
             </Label>
 
             <div class="flex gap-6">
-            <Button type="button" color="alternative" class="w-1/2" on:click={toggleToLogin}>Go Back</Button>
-            <Button type="button" class="w-1/2" on:click={signUpNewUser}>Create Account</Button>
+                <Button type="button" color="alternative" class="w-1/2" on:click={toggleToLogin}>Go Back</Button>
+                <Button type="button" class="w-1/2" on:click={signUpNewUser}>Create Account</Button>
             </div>
         </div>
     </Register>
