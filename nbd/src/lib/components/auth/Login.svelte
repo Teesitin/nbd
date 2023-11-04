f9f5040177b3efbd8f24efd76da3d3aa28c358f4
Teesitin <teesitin.russell@gmail.com>
2023-11-03 22:05:42 -0400

    Testing sign in

--- nbd/src/lib/components/auth/Login.svelte
+++ nbd/src/lib/components/auth/Login.svelte
@@ -2,11 +2,25 @@
     import { Section, Register } from 'flowbite-svelte-blocks';
     import { Button, Checkbox, Label, Input, Modal, Popover } from 'flowbite-svelte';
     import { createEventDispatcher } from 'svelte';
+    import { supabase } from '$lib/supabaseClient';
 
     const dispatch = createEventDispatcher();
 
+    let email = '';
+    let password ='';
+
+
     function toggleToSignUp() {
     dispatch('toggle', { newLoginState: false });
+    }
+
+
+    // Sign In
+    async function signIn() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+        email: 'example@email.com',
+        password: 'example-password',
+    })
     }
 
 </script>
@@ -16,22 +30,25 @@
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
         <form class="flex flex-col space-y-6" action="/">
             <h3 class="text-xl font-medium text-gray-900 dark:text-white p-0">Login</h3>
+
             <Label class="space-y-2">
-            <span>Email</span>
-            <Input type="email" name="email" placeholder="" required />
+                <span>Email</span>
+                <Input type="email" name="email" placeholder="" required bind:value={email}/>
             </Label>
+
             <Label class="space-y-2">
-            <span>Password</span>
-            <Input type="password" name="password" placeholder="" required />
+                <span>Password</span>
+                <Input type="password" name="password" placeholder="" required bind:value={password}/>
             </Label>
+
             <div class="flex items-start">
-            <Checkbox>Remember me</Checkbox>
-            <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot password?</a>
+                <Checkbox>Remember me</Checkbox>
+                <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Forgot password?</a>
             </div>
 
             <div class="flex gap-6">
                 <Button type="button" color="alternative" class="w-1/2"  on:click={toggleToSignUp}>Sign Up</Button>
-                <Button type="submit" class="w-1/2">Sign In</Button>
+                <Button type="button" class="w-1/2" on:click={signIn}>Sign In</Button>
             </div>
         </form>
         </div>
