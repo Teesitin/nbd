28fed8f36daf958b99f70c7a69c0c7b5cf98df6a
Teesitin <teesitin.russell@gmail.com>
2023-11-05 19:28:15 -0500

    Adding FAQ Answers

--- nbd/src/lib/components/page-bases/FAQBase.svelte
+++ nbd/src/lib/components/page-bases/FAQBase.svelte
@@ -6,17 +6,20 @@
   <Faq>
     <div>
       <FaqItem>
-        <svelte:fragment slot="h3">What does NDB do?</svelte:fragment>
-        <p class="text-gray-500 dark:text-gray-400">...</p>
+        <svelte:fragment slot="h3">What is Nothing But Doc?</svelte:fragment>
+        <p class="text-gray-500 dark:text-gray-400">The place to house all of your online documents.</p>
       </FaqItem>
+
       <FaqItem>
-        <svelte:fragment slot="h3">How does one become a Documentarian?</svelte:fragment>
-        <p class="text-gray-500 dark:text-gray-400">...</p>
+        <svelte:fragment slot="h3">How does one become a "Documentarian"?</svelte:fragment>
+        <p class="text-gray-500 dark:text-gray-400">That is too many words to put in a single paragraph, but we did try a whole page! Check out how to grade documentation</p>
+        <a href="/" class="font-medium  text-primary-600 dark:text-primary-500 hover:underline" target="_blank" rel="noreferrer">How To Doc</a>
       </FaqItem>
+
       <FaqItem>
         <svelte:fragment slot="h3">How does support work?</svelte:fragment>
-        <p class="text-gray-500 dark:text-gray-400">We are a very small team, but open-source! If you have any issues, throw it to our <a href="/" class="font-medium underline text-primary-600 dark:text-primary-500 hover:no-underline" target="_blank" rel="noreferrer">GitHub</a>!</p>
-
+        <p class="text-gray-500 dark:text-gray-400">We are a very small team, but open-source! If you have any issues, throw it to our <a href="/how-to-doc" class="font-medium text-primary-600 dark:text-primary-500 hover:underline" target="_blank" rel="noreferrer">GitHub</a>!</p>
+        <p class="text-gray-500 dark:text-gray-400">We will in the future have a dedicated email for support.</p>
       </FaqItem>
 
     </div>
@@ -24,7 +27,7 @@
       <FaqItem>
         <svelte:fragment slot="h3">What is your monetazation plan? Will you always be free?</svelte:fragment>
         <p class="text-gray-500 dark:text-gray-400">
-          Our core features will always be free and never locked behind a paywal overtime. Our current plan for monetazation is to provide a monthly service subscription to only privite your documentation lists. 
+          Our core features will always be free and never locked behind a paywall overtime. Our current plan for monetazation is to provide a monthly service subscription to only privite your documentation lists. 
         </p>
       </FaqItem>
       <FaqItem>
