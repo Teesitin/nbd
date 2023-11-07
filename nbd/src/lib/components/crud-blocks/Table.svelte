c683cd373d4b16a1f51f5709bf93006f92fb0484
Teesitin <teesitin.russell@gmail.com>
2023-11-07 16:23:40 -0500

    Update Table.svelte

--- nbd/src/lib/components/crud-blocks/Table.svelte
+++ nbd/src/lib/components/crud-blocks/Table.svelte
@@ -133,28 +133,31 @@
 <Section name="tableheader" sectionClass="bg-slate-50 dark:bg-transparent h-fit	 flex py-8 ">
     <TableHeader headerType="search">
         <Search slot="search" size="md" bind:value={searchTerm} />
-      <Button on:click={() => (formModal = true)} class="mx-6 my-3">
-        <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-          <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-        </svg>
-        Add Docs
-      </Button>
+
+        <Button on:click={() => (formModal = true)} class="mx-6 my-3">
+            <svg class="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
+            </svg>
+            Add Docs
+        </Button>
+
       <Button color="light">
         Actions&nbsp<ChevronDownOutline size="xs"/>
       </Button>
+
       <Dropdown>
-        <DropdownItem>Mass Edit</DropdownItem>
+        <DropdownItem><Checkbox bind:checked={enablePopover}>Popover</Checkbox></DropdownItem>
         <DropdownDivider />
-        <DropdownItem>Delete all</DropdownItem>
+        <DropdownItem>Dummy Action</DropdownItem>
       </Dropdown>
+
       <Button color="light">
-        <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="w-4 h-4 mr-2 text-gray-400" viewbox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-        </svg>
         Filter&nbsp<ChevronDownOutline size="xs"/>
       </Button>
+
       <Dropdown class="w-48 p-2 text-sm">
         <h6 class="mb-3 ml-1 text-sm font-medium text-gray-900 dark:text-white">Category</h6>
+        
         <li class="rounded p-1 hover:bg-gray-100 dark:hover:bg-gray-600">
           <Checkbox>Apple (56)</Checkbox>
         </li>
@@ -169,7 +172,6 @@
         </li>
       </Dropdown>
 
-      <Checkbox>Popover</Checkbox>
     </TableHeader>
 
     <Table class="mt-5">
@@ -204,9 +206,11 @@
 
   </Section>
 
-{#each filteredItems as row, index}
-  <Popover class="w-64 text-sm font-light " title="{row.title}" triggeredBy="#table-row-{index}">{row.description}</Popover>
-{/each}
+{#if enablePopover}
+    {#each filteredItems as row, index}
+    <Popover class="w-64 text-sm font-light " title="{row.title}" triggeredBy="#table-row-{index}">{row.description}</Popover>
+    {/each}
+{/if}
 
 
 
