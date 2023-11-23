fade9f3154f25cf4cde24d4c868612ac60a89883
Charity <90201079+CCRollins@users.noreply.github.com>
2023-11-23 17:29:56 -0500

    About Page

--- nbd/src/lib/components/page-bases/AboutBase.svelte
+++ nbd/src/lib/components/page-bases/AboutBase.svelte
@@ -9,10 +9,20 @@
         {
             href: '/',
             src: '/about/3-square.webp',
+            github: "https://github.com/Teesitin",
             alt: 'DeLayne Russell',
             name: 'DeLayne Russell',
             jobTitle: 'CEO & Web Developer',
             description: 'His biggest Dream is to complete this site. He likes Corned Beef and Cabbage.'
+        },
+        {
+            href: '/',
+            src: '/about/3-square.webp',
+            github: "https://github.com/CCRollins",
+            alt: 'Charity Rollins',
+            name: 'Charity Rollins',
+            jobTitle: 'Lost & Confused',
+            description: "She's DeLayne's twin until she figures out how this thing works. So far, she's only got her Github linked. This is only temporary."
         }
     ];
 
@@ -30,13 +40,13 @@
       <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">A dedicated team full of dedicated people</p>
     </TeamHeader>
     <TeamBody>
-      {#each members as { href, src, alt, name, jobTitle, description }}
+      {#each members as { href, src, alt, github, name, jobTitle, description }}
         <TeamItem {href} {src} {alt} {name} {jobTitle} imgClass="w-96 aspect-square">
           <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
             {description}
           </p>
           <svelte:fragment slot="social">
-            <Github href="https://github.com/Teesitin" />
+            <Github href={github} />
           </svelte:fragment>
         </TeamItem>
       {/each}
