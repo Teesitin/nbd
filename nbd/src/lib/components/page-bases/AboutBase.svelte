06fd4edec481f2b53c91717fe916a8ae5c0e6937
Charity <90201079+CCRollins@users.noreply.github.com>
2023-11-26 18:57:10 -0500

    Added members to about page

--- nbd/src/lib/components/page-bases/AboutBase.svelte
+++ nbd/src/lib/components/page-bases/AboutBase.svelte
@@ -1,45 +1,82 @@
 <script>
-    import { Section, ArticleAuthor, ArticleBody, ArticleHead, ArticleWrapper, BlogHead, BlogBodyWrapper } from 'flowbite-svelte-blocks';
-    import { VideoSolid, ArrowRightOutline, NewspaperSolid } from 'flowbite-svelte-icons';
+  import { Section, ArticleAuthor, ArticleBody, ArticleHead, ArticleWrapper, BlogHead, BlogBodyWrapper } from 'flowbite-svelte-blocks';
+  import { VideoSolid, ArrowRightOutline, NewspaperSolid } from 'flowbite-svelte-icons';
 
 
 
-    import {  TeamWrapper, TeamHeader, TeamBody, TeamItem, Facebook, Github, Twitter } from 'flowbite-svelte-blocks';
-    let members = [
-        {
-            href: '/',
-            src: '/about/3-square.webp',
-            alt: 'DeLayne Russell',
-            name: 'DeLayne Russell',
-            jobTitle: 'CEO & Web Developer',
-            description: 'His biggest Dream is to complete this site. He likes Corned Beef and Cabbage.'
-        }
-    ];
+  import {  TeamWrapper, TeamHeader, TeamBody, TeamItem, Facebook, Github, Twitter } from 'flowbite-svelte-blocks';
+  let members = [
+      {
+          href: '/',
+          src: '/about/3-square.webp',
+          github: "https://github.com/Teesitin",
+          alt: 'DeLayne Russell',
+          name: 'DeLayne Russell',
+          jobTitle: 'CEO & Web Developer',
+          description: 'His biggest Dream is to complete this site. He likes Corned Beef and Cabbage.'
+      },
+      {
+          href: '/',
+          src: '/about/charity.webp',
+          github: "https://github.com/CCRollins",
+          alt: 'Charity Rollins',
+          name: 'Charity Rollins',
+          jobTitle: 'Title',
+          description: "Description"
+      },
+      {
+          href: '/',
+          src: '/about/daniel.webp',
+          github: "https://github.com/DMosh235",
+          alt: 'Daniel Moshovetis',
+          name: 'Daniel Moshovetis',
+          jobTitle: 'Title',
+          description: 'Description'
+      },
+      {
+          href: '/',
+          src: '/about/tsehai.webp',
+          github: "https://github.com/tsehaiB",
+          alt: 'Tsehai Boucaud',
+          name: 'Tsehai Boucaud',
+          jobTitle: 'Title',
+          description: 'Description'
+      },
+      {
+          href: '/',
+          src: '/about/camila.webp',
+          github: "https://github.com/camzxx",
+          alt: 'Camila Florez',
+          name: 'Camila Florez',
+          jobTitle: 'Title',
+          description: 'Description'
+      }
+  ];
 
-  </script>
+</script>
+
+
+
+
+
   
-
-
-
-
-    
 <Section name="team">
-  <TeamWrapper>
-    <TeamHeader>
-      <svelte:fragment slot="label">The Team Behind Nothing But Docs</svelte:fragment>
-      <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">A dedicated team full of dedicated people</p>
-    </TeamHeader>
-    <TeamBody>
-      {#each members as { href, src, alt, name, jobTitle, description }}
-        <TeamItem {href} {src} {alt} {name} {jobTitle} imgClass="w-96 aspect-square">
-          <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
-            {description}
-          </p>
-          <svelte:fragment slot="social">
-            <Github href="https://github.com/Teesitin" />
-          </svelte:fragment>
-        </TeamItem>
-      {/each}
-    </TeamBody>
-  </TeamWrapper>
+<TeamWrapper>
+  <TeamHeader>
+    <svelte:fragment slot="label">The Team Behind Nothing But Docs</svelte:fragment>
+    <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">A dedicated team full of dedicated people</p>
+  </TeamHeader>
+  <TeamBody>
+    {#each members as { href, src, alt, github, name, jobTitle, description }}
+      <TeamItem {href} {src} {alt} {name} {jobTitle} imgClass="w-96 aspect-square">
+        <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+          {description}
+        </p>
+        <svelte:fragment slot="social">
+          <Github href={github} />
+        </svelte:fragment>
+      </TeamItem>
+    {/each}
+  </TeamBody>
+</TeamWrapper>
 </Section>
